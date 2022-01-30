fdc9d5c9e34f74b716a41b7b0c352b06f2e0e30c
IMRAN BAALI <imran.baali19@gmail.com>
2022-01-30 20:50:19 +0100

    (home) make the filter and sort working fine and put in in wilies card

--- src/Components/Home/WiliesCards/WiliesCards.jsx
+++ src/Components/Home/WiliesCards/WiliesCards.jsx
@@ -3,17 +3,80 @@ import WilyCard from "./WilyCard";
 import { useContext } from "react";
 import AuthContext from "../../../store/auth-context";
 import SearchFilter from "../../../Components/UI/SearchFilter";
+import { useState, useEffect } from "react";
+import {
+	convertTagsObjectsShapetoArray,
+	convertTagsObjectsNameShapetoArrayTags
+} from "../../../utils/Heplers";
 
 function WiliesCards(props) {
-	
 	const authCtx = useContext(AuthContext);
+	const { wilies } = props;
+	const [filteredWilies, setFilteredWilies] = useState([]);
+	const [sort, setSort] = useState("asc");
+	const [searchTags, setSearchTags] = useState(null);
+
+	useEffect(() => {
+		setFilteredWilies(wilies);
+	}, [wilies]);
+
+	const changeSortHandler = () => {
+		setSort((prevSort) => {
+			if (prevSort === "asc") {
+				setFilteredWilies((prevWilies) => {
+					const newWilies = [...prevWilies];
+					return newWilies.sort((a, b) => {
+						if (a.updatedAt > b.updatedAt) return 1;
+						else if (a.updatedAt < b.updatedAt) return -1;
+						else return 0;
+					});
+				});
+				return "desc";
+			} else prevSort === "desc";
+			{
+				setFilteredWilies((prevWilies) => {
+					const newWilies = [...prevWilies];
+					return newWilies.sort((a, b) => {
+						if (a.updatedAt > b.updatedAt) return -1;
+						else if (a.updatedAt < b.updatedAt) return 1;
+						else return 0;
+					});
+				});
+				return "asc";
+			}
+		});
+	};
+
+	const changeFilteredTags = (tags) => {
+		var filterTags = convertTagsObjectsShapetoArray(tags);
+		if (filterTags.length <= 0) setFilteredWilies(wilies);
+		else {
+			setFilteredWilies(() => {
+				return wilies.filter((wily) => {
+					var wilyTagsArray = convertTagsObjectsNameShapetoArrayTags(
+						wily.tags
+					);
+					return filterTags.every((element) => {
+						return wilyTagsArray.includes(element);
+					});
+				});
+			});
+		}
+		setSearchTags(tags);
+	};
 
 	return (
 		<>
-		<SearchFilter />
-		<div className="wilies-container container">
-			{
-				props.wilies.map(function qstAnswerMapped(wily) {
+			<SearchFilter
+				sort={sort}
+				setSort={setSort}
+				changeSortHandler={changeSortHandler}
+				searchTags={searchTags}
+				changeFilteredTags={changeFilteredTags}
+				suggestedTags={props.suggestedTags}
+			/>
+			<div className="wilies-container container">
+				{filteredWilies.map(function qstAnswerMapped(wily) {
 					return (
 						<WilyCard
 							key={wily._id}
@@ -23,9 +86,8 @@ function WiliesCards(props) {
 							deleteWilyHandler={props.deleteWilyHandler}
 						/>
 					);
-				})
-			}
-		</div>
+				})}
+			</div>
 		</>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,11 +10,7 @@ import useHttp from "../hooks/useHttp";
 import { useState, useEffect, useRef } from "react";
 import { useAlert } from "react-alert";
 import LoadingSpinner from "../Components/UI/LoadingSpinner";
-import {
-	convertTagsAraayObjsShapetoObjects,
-	convertTagsObjectsShapetoArray,
-	convertTagsObjectsNameShapetoArrayTags
-} from "../utils/Heplers";
+import { convertTagsAraayObjsShapetoObjects } from "../utils/Heplers";
 
 let isAlertExistBefore = false;
 
@@ -22,57 +18,10 @@ function Home() {
 	const authCtx = useContext(AuthContext);
 	const { isLoading, error, sendRequest: sendGetWiliesRequest } = useHttp();
 	const [wilies, setWilies] = useState([]);
-	const [filteredWilies, setFilteredWilies] = useState([]);
-	const [sort, setSort] = useState("asc");
-	const [searchTags, setSearchTags] = useState(null);
 	const [suggestedTags, setSuggestedTags] = useState([]);
-
-	const changeSortHandler = () => {
-		setSort((prevSort) => {
-			if (prevSort === "asc") {
-				setFilteredWilies((prevWilies) => {
-					const newWilies = [...prevWilies];
-					return newWilies.sort((a, b) => {
-						if (a.updatedAt > b.updatedAt) return 1;
-						else if (a.updatedAt < b.updatedAt) return -1;
-						else return 0;
-					});
-				});
-				return "desc";
-			} else prevSort === "desc";
-			{
-				setFilteredWilies((prevWilies) => {
-					const newWilies = [...prevWilies];
-					return newWilies.sort((a, b) => {
-						if (a.updatedAt > b.updatedAt) return -1;
-						else if (a.updatedAt < b.updatedAt) return 1;
-						else return 0;
-					});
-				});
-				return "asc";
-			}
-		});
-	};
-
-	const changeFilteredTags = (tags) => {
-		var filterTags = convertTagsObjectsShapetoArray(tags);
-		if (filterTags.length <= 0) setFilteredWilies(wilies);
-		else {
-			setFilteredWilies(() => {
-				return wilies.filter((wily) => {
-					var wilyTagsArray = convertTagsObjectsNameShapetoArrayTags(wily.tags);
-					return filterTags.every(element => {
-						return wilyTagsArray.includes(element);
-					})
-				});
-			});
-		}
-		setSearchTags(tags);
-	};
 
 	const getWilies = (data) => {
 		setWilies(data.wilies);
-		setFilteredWilies(data.wilies);
 	};
 
 	const getTags = (data) => {
@@ -110,9 +59,6 @@ function Home() {
 		setWilies((prevWilies) => {
 			return prevWilies.filter((wily) => wily._id !== wilyData.wily._id);
 		});
-		setFilteredWilies((prevWilies) => {
-			return prevWilies.filter((wily) => wily._id !== wilyData.wily._id);
-		});
 	};
 
 	const deleteWilyHandler = (wilyId) => {
@@ -134,10 +80,6 @@ function Home() {
 		if (isAlertExistBefore) alert.removeAll();
 		alert.show(wilyData.message, { type: "sucess", timeout: 3000 });
 		setWilies((prevWilies) => {
-			var newWilies = [...prevWilies, wilyData.wily];
-			return newWilies;
-		});
-		setFilteredWilies((prevWilies) => {
 			var newWilies = [...prevWilies, wilyData.wily];
 			return newWilies;
 		});
@@ -181,17 +123,10 @@ function Home() {
 			/>
 			{isLoading && <LoadingSpinner />}
 			<MainInputs ref={mainInputsRef} addWilyHandler={addWilyHandler} />
-			<SearchFilter
-				sort={sort}
-				setSort={setSort}
-				changeSortHandler={changeSortHandler}
-				searchTags={searchTags}
-				changeFilteredTags={changeFilteredTags}
-				suggestedTags={suggestedTags}
-			/>
 			<WiliesCards
 				deleteWilyHandler={deleteWilyHandler}
-				wilies={filteredWilies}
+				wilies={wilies}
+				suggestedTags={suggestedTags}
 			/>
 		</Layout>
 	);
