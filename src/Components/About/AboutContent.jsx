67876285f5ef0979c3d6ae74643f22bc16594b7b
Imran Baali <ibaali@e2r9p5.1337.ma>
2022-02-17 19:53:23 +0100

    add pricing section

--- src/Components/About/AboutContent.jsx
+++ src/Components/About/AboutContent.jsx
@@ -1,4 +1,6 @@
 import "./AboutContent.scss";
+import Twelve from "../../assets/teams/twelve.png";
+import kirwaKO from "../../assets/teams/kirwa-ko.png";
 
 function AboutContent() {
 	return (
@@ -6,11 +8,7 @@ function AboutContent() {
 			<div className="container content bg-dark-darkmode">
 				<h4>How to use Wil :</h4>
 				<p>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-					Blanditiis minima reprehenderit nisi eligendi. Maxime
-					explicabo, iusto est ipsum error autem libero distinctio
-					deleniti, vero accusamus soluta. Est blanditiis mollitia
-					non!
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, tenetur alias ullam veniam, soluta voluptate libero quibusdam eveniet quam nostrum laborum optio animi eligendi obcaecati exercitationem non explicabo omnis tempore?
 				</p>
 				<h4>Markdown ref :</h4>
 				<p>
@@ -51,6 +49,32 @@ function AboutContent() {
 						JANKI METHOD REFINED
 					</a>
 				</p>
+			</div>
+			<div className="container content bg-dark-darkmode">
+				<h4>Wil Pricing :</h4>
+				<p>wil is still in the development period, so we are not charging, for now, we would really like to hear your feedback (positive or negative), or if you have any ideas on how to improve the app</p>
+				<form className="bg-dark-darkmode border-none">
+					<div className="row">
+						<label htmlFor="email" className="w-100">Email :</label>
+						<input type="email" name="email" className="w-100" placeholder="youremail@gmail.com" />
+					</div>
+					<div className="row">
+						<label htmlFor="email" className="w-100">Feedback :</label>
+						<textarea name="" className="w-100" id="" rows="5" placeholder="your feedback..."></textarea>
+					</div>
+					<button className="main-btn">Submit</button>
+				</form>
+			</div>
+			<div className="container content bg-dark-darkmode mb-4">
+				<div className="row">
+					<div className="col-6">
+						<img src={Twelve} className="img-fluid" alt="" />
+						<h5 className="text-center mt-4">Abdelali Ait Hmid</h5>
+						<p className="text-center">Backend Developer</p>
+					</div>
+					<div className="col-6">
+					</div>
+				</div>
 			</div>
 		</div>
 	);
