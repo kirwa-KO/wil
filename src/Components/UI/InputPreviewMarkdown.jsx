f5fe179cdde4f292e45ee0f9f5e3fabb493598e8
Imran Baali <ibaali@e1r4p3.1337.ma>
2022-02-19 16:27:55 +0100

    (test mode) for react-md-editor

--- src/Components/UI/InputPreviewMarkdown.jsx
+++ src/Components/UI/InputPreviewMarkdown.jsx
@@ -1,5 +1,5 @@
 import "./InputPreviewMarkdown.scss";
-import Markdown from "./Markdown";
+// import Markdown from "./Markdown";
 // import MarkdownEditor from '@uiw/react-markdown-editor';
 import MDEditor from '@uiw/react-md-editor';
 
@@ -10,8 +10,8 @@ function InputPreviewMarkdown(props) {
 				value={props.value}
 				onChange={props.onChangeInput}
 				textareaProps={{
-						placeholder: props.placeholder,
-						maxLength: props.maxChars,
+					placeholder: props.placeholder,
+					maxLength: props.maxChars,
 				}}
 				height={props.height}
 			/>
