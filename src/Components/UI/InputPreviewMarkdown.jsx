16b865f23901646b3ca29115071f2259f9142250
Imran Baali <ibaali@e1r4p3.1337.ma>
2022-02-19 18:33:07 +0100

    (test mode) start using md-editor-rt

--- src/Components/UI/InputPreviewMarkdown.jsx
+++ src/Components/UI/InputPreviewMarkdown.jsx
@@ -1,12 +1,18 @@
 import "./InputPreviewMarkdown.scss";
 // import Markdown from "./Markdown";
-// import MarkdownEditor from '@uiw/react-markdown-editor';
-import MDEditor from '@uiw/react-md-editor';
+import Editor from 'md-editor-rt';
+import 'md-editor-rt/lib/style.css';
+import { useState } from "react";
 
 function InputPreviewMarkdown(props) {
+	const [text, setText] = useState('hello md-editor-rt！');
+	const [theme] = useState('light');
+
+	console.log(text);
+
 	return (
 		<div className={`row w-100 preview-container ${props.className}`}>
-			<MDEditor
+			{/* <MDEditor
 				value={props.value}
 				onChange={props.onChangeInput}
 				textareaProps={{
@@ -14,7 +20,22 @@ function InputPreviewMarkdown(props) {
 					maxLength: props.maxChars,
 				}}
 				height={props.height}
+			/> */}
+
+			<Editor
+				modelValue={text}
+				onChange={(modelValue) => {
+					setText(modelValue);
+				}}
+				// toolbarsExclude={['github', ]}
+				// theme={theme}
+				// editorClass="md-editor"
+				// language="en-US"
+				// style={{
+				// 	height: props.height,
+				// }}
 			/>
+
 		</div>
 	);
 }
